6dc1f8a76c280267c5e9b20bb107845a065102a9
britannic <github@empirecreekcircle.com>
2017-10-31 19:49:29 -0700

    Updated Excludes
    Minor typos and exclusions.

--- payload/update-dnsmasq.pl
+++ payload/update-dnsmasq.pl
@@ -2,7 +2,7 @@
 #
 # **** License ****
 #
-# Copyright (C) 2017 by Helmrock Consulting
+# Copyright (C) 2017 by Helm Rock Consulting
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
